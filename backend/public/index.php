a97e513aa2b4c31e826f9c5dda0bb68afef562c5
Leonardo <joelolaya681@gmail.com>
2026-01-08 11:45:03 -0500

    Detección de bugs

--- backend/public/index.php
+++ backend/public/index.php
@@ -9,10 +9,10 @@ $app->add(new \App\Middleware\AuthMiddleware());
 $app->add(new \App\Middleware\CorsMiddleware());
 
 // Carga de rutas
-require_once __DIR__ . '/../rest/usuarios.php';
-require_once __DIR__ . '/../rest/proyectos.php';
-require_once __DIR__ . '/../rest/tareas.php';
-require_once __DIR__ . '/../rest/datamaster.php';
-require_once __DIR__ . '/../rest/reportes.php';
+require_once __DIR__ . '/../rest/usuarios/usuarios.php';
+require_once __DIR__ . '/../rest/proyectos/proyectos.php';
+require_once __DIR__ . '/../rest/tareas/tareas.php';
+require_once __DIR__ . '/../rest/datamaster/datamaster.php';
+require_once __DIR__ . '/../rest/reportes/reportes.php';
 
 $app->run();
